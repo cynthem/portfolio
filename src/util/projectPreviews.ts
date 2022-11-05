caf8e79e140ad7a81bca61649b45d9c2d48dd5e1
doozles411 <cindy.phelps01@gmail.com>
2022-11-05 15:17:17 -0700

    Create and export projectPreviews

---
+++ src/util/projectPreviews.ts
@@ -0,0 +1,38 @@
+import projectImage from '../assets/images/projectPlaceholder.jpg';
+
+const projectPreviews = [
+    {
+        name: "To-Doing",
+        image: projectImage,
+        siteLink: "https://doozles411.github.io/todo-list-II/",
+        gitHubLink: "https://github.com/doozles411/todo-list-II",
+        description: "Description 3",
+        tools: "Built with TypeScript and Webpack"
+    },
+    {
+        name: "Battleship",
+        image: projectImage,
+        siteLink: "https://doozles411.github.io/battleship-II/",
+        gitHubLink: "https://github.com/doozles411/battleship-II",
+        description: "Description 4",
+        tools: "Built with jQuery and Webpack"
+    },
+    {
+        name: "ChillOut Co.",
+        image: projectImage,
+        siteLink: "https://doozles411.github.io/shopping-cart/",
+        gitHubLink: "https://github.com/doozles411/shopping-cart",
+        description: "Description 1",
+        tools: "Built with React and React Router"
+    },
+    {
+        name: "CV Creator",
+        image: projectImage,
+        siteLink: "https://doozles411.github.io/CV-app/",
+        gitHubLink: "https://github.com/doozles411/CV-app",
+        description: "Description 2",
+        tools: "Built with React"
+    }
+];
+
+export default projectPreviews;
