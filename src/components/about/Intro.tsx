bdc388c481fc693f635ce7dfba3ff43051de46b3
doozles411 <cindy.phelps01@gmail.com>
2022-11-04 19:35:32 -0700

    Create and export Intro

---
+++ src/components/about/Intro.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+function Intro() {
+    return (
+        <div className='intro'>
+            <div className='about-header' id='about'>
+                <p className='number'>01.</p>
+                <h2>About</h2>
+            </div>
+            <div className='intro-details'>
+                <p>details schmetails details schmetails details schmetails details schmetails details schmetails details schmetails details schmetails details schmetails details schmetails details schmetails details schmetails details schmetails details schmetails details schmetails details schmetails details schmetails details schmetails details schmetails details schmetails details schmetails</p>
+                <p>details schmetails details schmetails details schmetails details schmetails details schmetails details schmetails details schmetails details schmetails details schmetails details schmetails details schmetails details schmetails details schmetails details schmetails details schmetails details schmetails details schmetails details schmetails details schmetails details schmetails details schmetails details schmetails details schmetails details schmetails details schmetails details schmetails details schmetails details schmetails</p>
+                <p>details schmetails details schmetails details schmetails details schmetails details schmetails details schmetails details schmetails details schmetails details schmetails details schmetails details schmetails details schmetails details schmetails details schmetails details schmetails details schmetails details schmetails</p>
+            </div>
+        </div>
+    );
+}
+
+export default Intro;
