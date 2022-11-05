34b08f4a9cd38270978805f7f9cb94e055a21b4a
doozles411 <cindy.phelps01@gmail.com>
2022-11-05 16:08:31 -0700

    Create and export MoreProjects

---
+++ src/components/projects/MoreProjects.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function MoreProjects() {
+    return (
+        <div className='more-projects'>
+            more projects placeholder
+        </div>
+    )
+}
+
+export default MoreProjects;
