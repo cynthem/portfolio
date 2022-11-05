985562f0a23414ddad180af548f64858059b22ca
doozles411 <cindy.phelps01@gmail.com>
2022-11-05 14:33:25 -0700

    Import and render skills icons

--- src/components/skills/SkillsLeft.tsx
+++ src/components/skills/SkillsLeft.tsx
@@ -1,25 +1,6 @@
-import React from 'react';
-import skillIcons from '../../util/skillIcons';
 
-function SkillsLeft() {
-    return (
-        <div className='skills-left'>
-            <div className='box-container'>
-                <div className='box-row'>
-                    <div className='skill'>
-                        <img alt="HTML5" src={skillIcons[7]} />
-                        <p>HTML5</p>
-                    </div>
-                    <div className='skill'>
-                        <img alt="JavaScript" src={skillIcons[8]} />
-                        <p>JavaScript</p>
-                    </div>
-                </div>
-                <div className='box-row'>
-                    <div className='skill'>
-                        <img alt="CSS3" src={skillIcons[1]} />
-                        <p>CSS3</p>
-                    </div>
+                    
+                
                     <div className='skill react'>
                         <img alt="React" src={skillIcons[12]} />
                         <p>React</p>
