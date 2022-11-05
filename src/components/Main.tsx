89d074135d16b3f57b4d9500b07dbd5c96008b22
doozles411 <cindy.phelps01@gmail.com>
2022-11-05 14:34:56 -0700

    Restructure skills component

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import About from './about/About';
-import Skills from './skills/Skills';
+import Skills from './Skills';
 //import Projects from './projects/Projects';
 //import Contact from './contact/Contact';
 
