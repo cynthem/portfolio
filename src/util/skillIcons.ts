4f5e44cd64a871673e1adac13b3ed728f1328b79
doozles411 <cindy.phelps01@gmail.com>
2022-11-05 12:38:02 -0700

    Add jquery and typescript icons

--- src/util/skillIcons.ts
+++ src/util/skillIcons.ts
@@ -7,12 +7,14 @@ import github from '../assets/images/skill-icons/icon_github.svg';
 import html from '../assets/images/skill-icons/icon_html.svg';
 import javascript from '../assets/images/skill-icons/icon_javascript.svg';
 import jest from '../assets/images/skill-icons/icon_jest.svg';
+import jquery from '../assets/images/skill-icons/icon_jquery.svg'
 import nodejs from '../assets/images/skill-icons/icon_nodejs.svg';
 import python from '../assets/images/skill-icons/icon_python.svg';
 import react from '../assets/images/skill-icons/icon_react.svg';
 import redux from '../assets/images/skill-icons/icon_redux.svg';
 import sass from '../assets/images/skill-icons/icon_sass.svg';
 import styled from '../assets/images/skill-icons/icon_styledComponents.svg';
+import typescript from '../assets/images/skill-icons/icon_typescript.svg'
 import webpack from '../assets/images/skill-icons/icon_webpack.svg';
 
 const skillIcons = [
@@ -25,12 +27,14 @@ const skillIcons = [
     html,
     javascript,
     jest,
+    jquery,
     nodejs,
     python,
     react,
     redux,
     sass,
     styled,
+    typescript,
     webpack
 ];
 
