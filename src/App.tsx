89cdbe051a93c641231ae60f456d1e708c9b5c4f
doozles411 <cindy.phelps01@gmail.com>
2022-11-04 18:23:51 -0700

    Import LeftPanel, Main and Footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,24 @@
 import React, { useState } from 'react';
+import LeftPanel from './components/LeftPanel';
+import Main from './components/Main';
+import Footer from './components/Footer';
 import './sass/main.scss';
 
 export const App: React.FC = () => {
-  const [inView, setInView] = useState(0);
+  const [inView, setInView] = useState<Number | undefined>(0);
 
-  function handleView(view) {
+  function handleView(view: Number) {
     setInView(view);
   }
 
   return (
     <div className='app'>
-
+      <LeftPanel 
+        //inView={inView}
+        //handleView={handleView}
+      />
+      <Main />
+      <Footer />
     </div>
   );
 }
