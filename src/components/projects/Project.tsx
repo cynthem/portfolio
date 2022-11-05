600dd8aa4da2e74256e4f29078a591c955a12d9b
doozles411 <cindy.phelps01@gmail.com>
2022-11-05 15:32:31 -0700

    Upload image

---
+++ src/components/projects/Project.tsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import arrowIcon from '../../assets/images/icon_arrow.png';
+
+function Project({ project }) {
+    return (
+        <div className='project-container'>
+            <img 
+                alt={project.name}
+                src={project.image}
+            />
+            <div className='project-card'>
+                <h3>{project.name}</h3>
+                <div className='card-container'>
+                    <div className='card-description'>
+                        {project.description}
+                    </div>
+                    <div className='break'></div>
+                    <div className='card-tools'>
+                        {project.tools}
+                    </div>
+                    <div className='card-links'>
+                        <a href={project.siteLink}>
+                            Live
+                            <img alt="Arrow icon" src={arrowIcon} />
+                        </a>
+                        <a href={project.gitHubLink}>
+                            Code
+                            <img alt="Arrow icon" src={arrowIcon} />
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Project;
