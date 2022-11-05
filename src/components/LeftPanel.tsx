ee3e30af563f0e75d2a0b1bc0d35f45a9c5057f8
doozles411 <cindy.phelps01@gmail.com>
2022-11-04 18:09:44 -0700

    Render initial text

---
+++ src/components/LeftPanel.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import views from '../util/pageViews';
+
+function LeftPanel() {
+    return (
+        <div className='left-panel'>
+            left panel
+        </div>
+    );
+}
+
+export default LeftPanel;
