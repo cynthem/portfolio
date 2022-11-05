3cde1ccdad2ade5bef45a6f74dbfec10e2ecbe13
doozles411 <cindy.phelps01@gmail.com>
2022-11-05 12:26:04 -0700

    Create skillIcons

---
+++ src/util/skillIcons.ts
@@ -0,0 +1,37 @@
+import bootstrap from '../assets/images/skill-icons/icon_bootstrap.svg';
+import css from '../assets/images/skill-icons/icon_css.svg';
+import firebase from '../assets/images/skill-icons/icon_firebase.svg';
+import framer from '../assets/images/skill-icons/icon_framer.svg';
+import git from '../assets/images/skill-icons/icon_git.svg';
+import github from '../assets/images/skill-icons/icon_github.svg';
+import html from '../assets/images/skill-icons/icon_html.svg';
+import javascript from '../assets/images/skill-icons/icon_javascript.svg';
+import jest from '../assets/images/skill-icons/icon_jest.svg';
+import nodejs from '../assets/images/skill-icons/icon_nodejs.svg';
+import python from '../assets/images/skill-icons/icon_python.svg';
+import react from '../assets/images/skill-icons/icon_react.svg';
+import redux from '../assets/images/skill-icons/icon_redux.svg';
+import sass from '../assets/images/skill-icons/icon_sass.svg';
+import styled from '../assets/images/skill-icons/icon_styledComponents.svg';
+import webpack from '../assets/images/skill-icons/icon_webpack.svg';
+
+const skillIcons = [
+    bootstrap,
+    css,
+    firebase,
+    framer,
+    git,
+    github,
+    html,
+    javascript,
+    jest,
+    nodejs,
+    python,
+    react,
+    redux,
+    sass,
+    styled,
+    webpack
+];
+
+export default skillIcons;
