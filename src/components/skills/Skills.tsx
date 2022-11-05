12e101a499d0d3113d64cb4be8c5cd46e391cada
doozles411 <cindy.phelps01@gmail.com>
2022-11-05 12:21:25 -0700

    Create and export Skills

---
+++ src/components/skills/Skills.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import SkillsLeft from './SkillsLeft';
+import SkillsRight from './SkillsRight';
+
+function Skills() {
+    return (
+        <div className='skills'>
+            <div className='skills-header' id='skills'>
+                <p className='number'>02.</p>
+                <h2>Skills</h2>
+            </div>
+            <div className='skills-boxes'>
+                <SkillsLeft />
+                <SkillsRight />
+            </div>
+        </div>
+    );
+}
+
+export default Skills;
