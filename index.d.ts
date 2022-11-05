fd274872ed537f0b2e7ab7aabbf50e8f7fd96861
doozles411 <cindy.phelps01@gmail.com>
2022-11-04 19:50:20 -0700

    Adjust settings

--- index.d.ts
+++ index.d.ts
@@ -1,3 +1,14 @@
-declare module "*.jpg";
-declare module "*.png";
-declare module "*.jpeg";
+declare module "*.jpg" {
+    const path: string;
+    export default path;
+}
+
+declare module "*.jpeg" {
+    const path: string;
+    export default path;
+}
+
+declare module "*.png" {
+    const path: string;
+    export default path;
+}
