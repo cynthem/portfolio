00507a3745859a44fbda79c56dbe3cd68fd433c7
doozles411 <cindy.phelps01@gmail.com>
2022-11-05 13:28:14 -0700

    Remove container div

--- src/components/about/Heading.tsx
+++ src/components/about/Heading.tsx
@@ -8,19 +8,17 @@ function Heading() {
                 <h1 className='first'>Cindy</h1>
                 <h1 className='last'>Phelps</h1>
             </div>
-            <div className='heading-container'>
-                <div className='selfie'>
-                    <div className='selfie-outline'></div>
-                    <img 
-                        className='selfie-img'
-                        alt="Selfie"
-                        src={selfie}
-                    />
-                </div>
-                <div className='job-title'>
-                    <p>Hello! I am a</p>
-                    <h2>Front-end Developer</h2>
-                </div>
+            <div className='selfie'>
+                <div className='selfie-outline'></div>
+                <img 
+                    className='selfie-img'
+                    alt="Selfie"
+                    src={selfie}
+                />
+            </div>
+            <div className='job-title'>
+                <p>Hello! I am a</p>
+                <h2>Front-end Developer</h2>
             </div>
         </div>
         /*  <div className='image-title'>
