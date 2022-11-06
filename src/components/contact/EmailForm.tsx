180d748862fd62b07371e6f84d675f0c76797e3b
doozles411 <cindy.phelps01@gmail.com>
2022-11-06 13:44:15 -0800

    Create and export EmailForm

---
+++ src/components/contact/EmailForm.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import arrowIcon from '../../assets/images/arrow_icon.png';
+
+function EmailForm() {
+    return (
+        <form className='email-form'>
+            <div className='form-inputs'>
+                <input className='name-input' type='text' placeholder='Name'/>
+                <input className='email-input' type='text' placeholder='Email'/>
+                <textarea className='message-input' placeholder='Message'></textarea>
+            </div>
+            <button className='send-input'>
+                Send
+                <img alt='Send' src={arrowIcon} />
+            </button>
+        </form>
+    )
+}
+
+export default EmailForm;
