90a4388d0c90e1916996562c3db2a56505e28585
doozles411 <cindy.phelps01@gmail.com>
2022-11-06 13:42:59 -0800

    Create and export Details

---
+++ src/components/contact/Details.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Details() {
+    return (
+        <div className='details'>
+            <p>Info details and resume link <a href='placeholder'>resume</a></p>
+        </div>
+    )
+}
+
+export default Details;
