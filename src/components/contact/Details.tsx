927253c3991136f91b0b6daf53747d79d15a4eec
doozles411 <cindy.phelps01@gmail.com>
2022-11-06 14:15:19 -0800

    Change icon size to 50px

--- src/components/contact/Details.tsx
+++ src/components/contact/Details.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
-import emailIcon from '../../assets/images/contact-icons/email_icon.svg';
 import gitHubIcon from '../../assets/images/contact-icons/github_icon.svg';
 import linkedInIcon from '../../assets/images/contact-icons/linkedin_icon.svg';
+import emailIcon from '../../assets/images/contact-icons/email_icon.svg';
+import resumeIcon from '../../assets/images/contact-icons/resume_icon.svg';
 
 function Details() {
     return (
@@ -10,6 +11,8 @@ function Details() {
             <div className='details-links'>
                 <img alt="GitHub" src={gitHubIcon} />
                 <img alt="LinkedIn" src={linkedInIcon} />
+                <img alt="Email" src={emailIcon} />
+                <img alt="Resume" src={resumeIcon} />
             </div>
         </div>
     )
