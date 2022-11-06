f46716e714d4b2067cc5f7091337afb145f29090
doozles411 <cindy.phelps01@gmail.com>
2022-11-06 14:07:36 -0800

    Import and render GitHub icon

--- src/components/contact/Details.tsx
+++ src/components/contact/Details.tsx
@@ -1,9 +1,15 @@
 import React from 'react';
+import emailIcon from '../../assets/images/contact-icons/email_icon.svg';
+import gitHubIcon from '../../assets/images/contact-icons/github_icon.svg';
+import linkedInIcon from '../../assets/images/contact-icons/linkedin_icon.svg';
 
 function Details() {
     return (
         <div className='details'>
-            <p>Info details and resume link <a href='placeholder'>resume</a></p>
+            <p>Info details and resume link <a href='placeholder'>resume</a>.</p>
+            <div className='details-links'>
+                <img alt="GitHub" src={gitHubIcon} />
+            </div>
         </div>
     )
 }
