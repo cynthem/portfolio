5f3784588b405f669c4e295b3202f77eb9c59e87
doozles411 <cindy.phelps01@gmail.com>
2022-12-17 16:26:03 -0800

    Import motion from framer-motion

--- src/components/LeftPanel.tsx
+++ src/components/LeftPanel.tsx
@@ -1,16 +1,19 @@
 import React from 'react';
 import views from '../util/pageViews';
+import { motion } from "framer-motion"
 
 function LeftPanel() {
     return (
-        <div className='left-panel'>
-            <ul className='link-list'>
-                <li><a href="#about">About</a></li>
-                <li><a href="#skills">Skills</a></li>
-                <li><a href="#projects">Projects</a></li>
-                <li><a href="#contact">Contact</a></li>
-            </ul>
-        </div>
+        <motion.div>
+            <div className='left-panel'>
+                <ul className='link-list'>
+                    <li><a href="#about">About</a></li>
+                    <li><a href="#skills">Skills</a></li>
+                    <li><a href="#projects">Projects</a></li>
+                    <li><a href="#contact">Contact</a></li>
+                </ul>
+            </div>
+        </motion.div>
     );
 }
 
