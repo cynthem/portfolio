061ac1585e9e6973aaf22927548484bf36421f39
doozles411 <cindy.phelps01@gmail.com>
2022-12-18 13:49:54 -0800

    Remove initial animation

--- src/components/LeftPanel.tsx
+++ src/components/LeftPanel.tsx
@@ -4,22 +4,14 @@ import { motion } from "framer-motion"
 
 function LeftPanel() {
     return (
-        <motion.div 
-            animate={{ x: ['-100px', '0px'] }}
-            transition={{ 
-                delay: 0.5,
-                x: { duration: 1 } 
-            }}
-        >
-            <div className='left-panel'>
-                <ul className='link-list'>
-                    <li><a href="#about">About</a></li>
-                    <li><a href="#skills">Skills</a></li>
-                    <li><a href="#projects">Projects</a></li>
-                    <li><a href="#contact">Contact</a></li>
-                </ul>
-            </div>
-        </motion.div>
+        <div className='left-panel'>
+            <ul className='link-list'>
+                <li><a href="#about">About</a></li>
+                <li><a href="#skills">Skills</a></li>
+                <li><a href="#projects">Projects</a></li>
+                <li><a href="#contact">Contact</a></li>
+            </ul>
+        </div>
     );
 }
 
