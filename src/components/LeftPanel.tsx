35cd4f90e1e07464d752444793d15f0645511811
doozles411 <cindy.phelps01@gmail.com>
2022-12-19 17:27:14 -0800

    Create Props interface

--- src/components/LeftPanel.tsx
+++ src/components/LeftPanel.tsx
@@ -2,7 +2,11 @@ import React from 'react';
 import views from '../util/pageViews';
 import { motion } from "framer-motion"
 
-function LeftPanel({scrollView}) {
+interface Props {
+    scrollView: number;
+}
+
+function LeftPanel(props:Props) {
     return (
         <div className='left-panel'>
             <ul className='link-list'>
