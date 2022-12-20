cc710d813975f3c559f53f7967543088da49c83d
doozles411 <cindy.phelps01@gmail.com>
2022-12-19 18:04:36 -0800

    Remove link tags

--- src/components/LeftPanel.tsx
+++ src/components/LeftPanel.tsx
@@ -1,4 +1,4 @@
-import React, { Dispatch, SetStateAction } from 'react';
+import React, { useEffect, Dispatch, SetStateAction } from 'react';
 import views from '../util/pageViews';
 import { motion } from "framer-motion"
 
@@ -8,14 +8,28 @@ interface Props {
     setCurrentPage: Dispatch<SetStateAction<string>>;
 }
 
-function LeftPanel(props: Props) {
+function LeftPanel({ scrollView, currentPage, setCurrentPage }: Props) {
+    useEffect(() => {
+        if (scrollView > 10) {
+            setCurrentPage("about");
+        } else if (scrollView > 20) {
+            setCurrentPage("skills");
+        } else if (scrollView > 30) {
+            setCurrentPage("projects");
+        } else if (scrollView > 40) {
+            setCurrentPage("contact");
+        } else {
+            setCurrentPage("home");
+        }
+    }, [scrollView]);
+
     return (
         <div className='left-panel'>
             <ul className='link-list'>
-                <li><a href="#about">About</a></li>
-                <li><a href="#skills">Skills</a></li>
-                <li><a href="#projects">Projects</a></li>
-                <li><a href="#contact">Contact</a></li>
+                <li>About</li>
+                <li>Skills</li>
+                <li>Projects</li>
+                <li>Contact</li>
             </ul>
         </div>
     );
