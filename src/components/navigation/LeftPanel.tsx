d65c0c631c546eb9802e0afcdc7a71527d58a4b2
doozles411 <cindy.phelps01@gmail.com>
2022-12-21 14:58:00 -0800

    Create handleScroll

--- src/components/navigation/LeftPanel.tsx
+++ src/components/navigation/LeftPanel.tsx
@@ -8,12 +8,12 @@ interface Props {
     setCurrentPage: Dispatch<SetStateAction<string>>;
 }
 
-const navItems = {
-    Top: 0,
-    AboutSection: null,
-    SkillsSection: null, 
-    ProjectsSection: null, 
-    ContactSection: null
+const navItems: { [key: string]: number | null } = {
+        Top: 0,
+        AboutSection: null,
+        SkillsSection: null, 
+        ProjectsSection: null, 
+        ContactSection: null
 }
 
 function LeftPanel({ scrollView, currentPage, setCurrentPage }: Props) {
@@ -42,11 +42,34 @@ function LeftPanel({ scrollView, currentPage, setCurrentPage }: Props) {
         window.addEventListener('scroll', handleScroll);
     }, []);
 
-    const getAnchorPoints = () => {}
+    const getAnchorPoints = () => {
+        const currentScroll = window.scrollY - 100;
+        const viewportHeight = Math.max(
+            document.documentElement.clientHeight,
+            window.innerHeight || 0
+        );
+        for (const key in navItems) {
+            navItems[key] = document.getElementById(key)?.getBoundingClientRect().top! + currentScroll;
+        }
+        const bottom = document.body.offsetHeight;
+        handleScroll();
+    }
 
-    const handleScroll = () => {}
+    const handleScroll = () => {
+        const currentPosition = window.scrollY;
+        let currentSection = null;
+        for (const section in navItems) {
+            currentSection = navItems[section]! >= currentPosition ? section : currentSection;
+            if (currentSection !== section) {
+                break;
+            }
+        }
+        if (currentSection !== activeNav) {
+            setActiveNav(currentSection!);
+        }
+    }
 
-    const navList = Object.keys(sections).map((e, i) => 
+    const navList = Object.keys(navItems).map((e, i) => 
         <NavItem navName={e} key={`navitem_${i}`} active={e === activeNav ? true : false} currentPage={currentPage} />
     );
 
