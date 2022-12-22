6d995c8dfffa140bac738eb4d55f63769187b80e
doozles411 <cindy.phelps01@gmail.com>
2022-12-21 20:50:02 -0800

    Create useEffect

--- src/components/projects/Projects.tsx
+++ src/components/projects/Projects.tsx
@@ -1,10 +1,27 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { motion } from 'framer-motion';
 import uniqid from 'uniqid';
 import Project from './Project';
 import MoreProjects from './MoreProjects';
 import projectPreviews from '../../util/projectPreviews';
 
-function Projects() {
+interface Props {
+    scrollView: number;
+}
+
+function Projects({ scrollView }: Props) {
+    const [inView, setInView] = useState(false);
+
+    useEffect(() => {
+        if (scrollView < 35) {
+            setInView(false);
+        } else if (scrollView >= 35 && scrollView <= 94) {
+            setInView(true);
+        } else if (scrollView > 94) {
+            setInView(false);
+        }
+    }, [scrollView]);
+
     return (
         <div className='projects' id='projects'>
             <div className='projects-header'>
