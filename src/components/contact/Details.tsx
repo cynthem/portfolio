6e4c92ea2b075d4164e08818c436559930418ac8
doozles411 <cindy.phelps01@gmail.com>
2022-12-22 12:35:03 -0800

    Add link details

--- src/components/contact/Details.tsx
+++ src/components/contact/Details.tsx
@@ -9,10 +9,18 @@ function Details() {
         <div className='details'>
             <p>Info details and resume link <a href='placeholder'>resume</a>. More content to display what this would look like with a few sentences instead of just one short one for styling purposes.</p>
             <div className='details-links'>
-                <a href="#" className='github-link'>
+                <a 
+                    href="https://github.com/doozles411"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
                     <img alt="GitHub" src={gitHubIcon} />
                 </a>
-                <a href="#">
+                <a 
+                    href="#"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
                     <img alt="LinkedIn" src={linkedInIcon} />
                 </a>
                 <a href="#">
