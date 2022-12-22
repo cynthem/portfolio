6e25f008d1d23844c151a59a368f09e82910c634
doozles411 <cindy.phelps01@gmail.com>
2022-12-22 12:24:48 -0800

    Add hover and active styles to links

--- src/components/contact/Details.tsx
+++ src/components/contact/Details.tsx
@@ -9,7 +9,7 @@ function Details() {
         <div className='details'>
             <p>Info details and resume link <a href='placeholder'>resume</a>. More content to display what this would look like with a few sentences instead of just one short one for styling purposes.</p>
             <div className='details-links'>
-                <a href="#">
+                <a href="#" className='github-link'>
                     <img alt="GitHub" src={gitHubIcon} />
                 </a>
                 <a href="#">
