2b60d2506f71ef35685614aadfb6531f80dbd7bc
doozles411 <cindy.phelps01@gmail.com>
2022-12-21 19:40:09 -0800

    Render component conditionally

--- src/components/about/About.tsx
+++ src/components/about/About.tsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import { motion } from 'framer-motion';
 import Heading from './Heading';
 import Intro from './Intro';
 
@@ -10,19 +11,33 @@ function About({ scrollView }: Props) {
     const [inView, setInView] = useState(true);
 
     useEffect(() => {
-        if (scrollView < 20) {
+        if (scrollView < 19) {
             setInView(true);
         } else {
             setInView(false);
         }
     }, [scrollView]);
 
+    const aboutInView = <motion.div
+            className='about-background'
+            animate={{ scale: 1 }}
+        >
+            <Heading />
+            <Intro />
+        </motion.div>
+    
+    const aboutOutView = <motion.div
+            className='about-background'
+            style={{ originX: 1 }}
+            animate={{ scale: 0.8 }}
+        >
+            <Heading />
+            <Intro />
+        </motion.div>
+
     return (
         <div className='about'>
-            <div className='about-background'>
-                <Heading />
-                <Intro />
-            </div>
+            {inView ? aboutInView : aboutOutView}
         </div>
     );
 }
