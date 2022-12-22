cbf5370355c24c0cfbcab59dc56c57f10a021b8d
doozles411 <cindy.phelps01@gmail.com>
2022-12-22 15:27:42 -0800

    Import and render heart icon

--- src/components/contact/Footer.tsx
+++ src/components/contact/Footer.tsx
@@ -1,9 +1,24 @@
 import React from 'react';
+import heartIcon from '../../assets/images/heart_icon.svg';
 
 function Footer() {
     return (
         <div className='footer'>
-            <p>Made with 3 by <a href='https://github.com/doozles411' target="_blank" rel="noopener noreferrer">CMP</a></p>
+            <p>
+                Made with 
+                <img 
+                    alt='love'
+                    src={heartIcon}
+                /> 
+                by 
+                <a 
+                    href='https://github.com/doozles411' 
+                    target="_blank" 
+                    rel="noopener noreferrer"
+                >
+                    CMP
+                </a>
+            </p>
         </div>
     );
 }
