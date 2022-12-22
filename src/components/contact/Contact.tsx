7a361621acfd22bd38faf2765e79cbbecb19973b
doozles411 <cindy.phelps01@gmail.com>
2022-12-22 11:49:13 -0800

    Add animations

--- src/components/contact/Contact.tsx
+++ src/components/contact/Contact.tsx
@@ -5,16 +5,52 @@ import Details from './Details';
 import EmailForm from './EmailForm';
 
 function Contact() {
+    const animation = useAnimation();
+    const { ref, inView } = useInView({ threshold: 0.6 });
+
+    useEffect(() => {
+        if (inView) {
+            animation.start('visible');
+        }
+    }, [inView]);
+
+    const animateDetails = {
+        visible: {
+            y: 0,
+            opacity: 1,
+            staggerChildren: 1,
+            delayChildren: 1,
+            transition: {
+                duration: 1.5
+            }
+        },
+        hidden: {
+            y: '10vw',
+            opacity: 0
+        }
+    }
+
     return (
-        <div className='contact'>
-            <div className='contact-header' id='contact'>
+        <div className='contact' ref={ref}>
+            <motion.div 
+                className='contact-header' 
+                id='contact'
+                initial='hidden'
+                animate={animation}
+                variants={animateDetails}
+            >
                 <p className='number'>04.</p>
                 <h2>Contact</h2>
-            </div>
-            <div className='contact-details'>
+            </motion.div>
+            <motion.div 
+                className='contact-details'
+                initial='hidden'
+                animate={animation}
+                variants={animateDetails}
+            >
                 <Details />
                 <EmailForm />
-            </div>
+            </motion.div>
         </div>
     );
 }
