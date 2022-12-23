a7fbf010e15ee0b4f540cdc1c60cf0f7f35eacd5
doozles411 <cindy.phelps01@gmail.com>
2022-12-22 18:09:23 -0800

    Import and render images

--- src/components/projects/MoreProjects.tsx
+++ src/components/projects/MoreProjects.tsx
@@ -1,10 +1,24 @@
 import React from 'react';
+import etchImg from '../../assets/images/project-imgs/etch.jpg';
+import weatherImg from '../../assets/images/project-imgs/weather.jpg';
+import signupImg from '../../assets/images/project-imgs/signup.jpg';
 
 function MoreProjects() {
     return (
         <div className='more-projects'>
             <div className='more-container'>
-                <img />
+                <img 
+                    alt='More projects'
+                    src={weatherImg}
+                />
+                <img 
+                    alt='More projects'
+                    src={etchImg}
+                />
+                <img 
+                    alt='More projects'
+                    src={signupImg}
+                />
             </div>
         </div>
     )
