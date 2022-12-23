23a7c5bc7638cb56374f484a39970f2dd56bc133
doozles411 <cindy.phelps01@gmail.com>
2022-12-22 16:52:02 -0800

    Add link attributes

--- src/components/projects/Project.tsx
+++ src/components/projects/Project.tsx
@@ -11,19 +11,23 @@ function Project({ project }: { project: any }) {
             <div className='project-card'>
                 <h3>{project.name}</h3>
                 <div className='card-container'>
-                    <div className='card-description'>
-                        {project.description}
-                    </div>
+                    <div className='card-description'>{project.description}</div>
                     <div className='break'></div>
-                    <div className='card-tools'>
-                        {project.tools}
-                    </div>
+                    <div className='card-tools'>{project.tools}</div>
                     <div className='card-links'>
-                        <a href={project.siteLink}>
+                        <a 
+                            href={project.siteLink}
+                            target='_blank'
+                            rel='noopener noreferrer'
+                        >
                             Live
                             <img alt="Arrow icon" src={arrowIcon} />
                         </a>
-                        <a href={project.gitHubLink}>
+                        <a 
+                            href={project.gitHubLink}
+                            target='_blank'
+                            rel='noopener noreferrer'
+                        >
                             Code
                             <img alt="Arrow icon" src={arrowIcon} />
                         </a>
