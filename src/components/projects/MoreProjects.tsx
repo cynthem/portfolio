30de2e2b973ca82eca15a3e47aca0d9fa7d6181f
doozles411 <cindy.phelps01@gmail.com>
2022-12-22 18:14:40 -0800

    Add link

--- src/components/projects/MoreProjects.tsx
+++ src/components/projects/MoreProjects.tsx
@@ -1,24 +1,40 @@
 import React from 'react';
-import etchImg from '../../assets/images/project-imgs/etch.jpg';
 import weatherImg from '../../assets/images/project-imgs/weather.jpg';
+import etchImg from '../../assets/images/project-imgs/etch.jpg';
 import signupImg from '../../assets/images/project-imgs/signup.jpg';
+import arrowIcon from '../../assets/images/arrow_icon.png';
 
 function MoreProjects() {
     return (
         <div className='more-projects'>
             <div className='more-container'>
-                <img 
-                    alt='More projects'
-                    src={weatherImg}
-                />
-                <img 
-                    alt='More projects'
-                    src={etchImg}
-                />
-                <img 
-                    alt='More projects'
-                    src={signupImg}
-                />
+                <div className='more-imgs'>
+                    <img 
+                        alt='More projects'
+                        src={weatherImg}
+                    />
+                    <img 
+                        alt='More projects'
+                        src={etchImg}
+                    />
+                    <img 
+                        alt='More projects'
+                        src={signupImg}
+                    />
+                </div>
+                <div className='more-link'>
+                    <a
+                        href='http://github.com/doozles411?tab=repositories'
+                        target='_blank'
+                        rel='noopener noreferrer'
+                    >
+                        View more projects
+                        <img 
+                            alt='View more projects'
+                            src={arrowIcon}
+                        />
+                    </a>
+                </div>
             </div>
         </div>
     )
