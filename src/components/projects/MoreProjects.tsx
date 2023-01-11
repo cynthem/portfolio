e0dfb7e85e874bfc1ad0d2184c7af1f0f9d6a9d2
doozles411 <cindy.phelps01@gmail.com>
2023-01-10 16:17:20 -0800

    Add elements

--- src/components/projects/MoreProjects.tsx
+++ src/components/projects/MoreProjects.tsx
@@ -1,10 +1,19 @@
 import React from 'react';
 import otherProjects from '../../assets/images/project-imgs/otherProj.jpg';
+import arrowIcon from '../../assets/images/arrow_icon.png';
 
 function MoreProjects() {
     return (
         <div className='more-projects'>
-
+            <div className='more-projects-outline'></div>
+            <img 
+                alt='More projects' 
+                src={otherProjects}
+            />
+            <div className='more-projects-link'>
+                <h3>More projects</h3>
+                <img alt="Arrow icon" src={arrowIcon} />
+            </div>
         </div>
     )
 }
