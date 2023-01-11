241297e0eabddab86029cff553e5e681c79f233b
doozles411 <cindy.phelps01@gmail.com>
2023-01-10 16:32:17 -0800

    Nest image in background div

--- src/components/projects/MoreProjects.tsx
+++ src/components/projects/MoreProjects.tsx
@@ -5,11 +5,12 @@ import arrowIcon from '../../assets/images/arrow_icon.png';
 function MoreProjects() {
     return (
         <div className='more-projects'>
-            <div className='more-projects-outline'></div>
-            <img 
-                alt='More projects' 
-                src={otherProjects}
-            />
+            <div className='more-projects-background'>
+                <img 
+                    alt='More projects' 
+                    src={otherProjects}
+                />
+            </div>
             <div className='more-projects-link'>
                 <h3>More projects</h3>
                 <img alt="Arrow icon" src={arrowIcon} />
