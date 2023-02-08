f24636063c0495f460daf9fe13d37b5f4593593a
doozles411 <cindy.phelps01@gmail.com>
2023-02-07 16:42:46 -0800

    Remove hover and active styles

--- src/components/contact/Details.tsx
+++ src/components/contact/Details.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { motion } from 'framer-motion';
 import gitHubIcon from '../../assets/images/contact-icons/github_icon.svg';
 import linkedInIcon from '../../assets/images/contact-icons/linkedin_icon.svg';
 import resumeIcon from '../../assets/images/contact-icons/resume_icon.svg';
@@ -8,13 +9,18 @@ function Details() {
         <div className='details'>
             <p>I'm currently open to new opportunities! Don't hesitate to reach out if you're interested in working together, have any questions, or just want to drop me a line.</p>
             <div className='details-links'>
-                <a 
+                <motion.a 
                     href="https://github.com/doozles411"
                     target="_blank"
                     rel="noopener noreferrer"
+                    whileHover={{
+                        scale: 1.1,
+                        transition: { duration: 0.5 }
+                    }}
+                    whileTap={{ scale: 0.9 }}
                 >
                     <img alt="GitHub" src={gitHubIcon} />
-                </a>
+                </motion.a>
                 <a 
                     href="#"
                     target="_blank"
