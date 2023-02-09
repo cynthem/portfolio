0a1737fe3c6ec3acb48c9e9f0e30ea64453a0603
doozles411 <cindy.phelps01@gmail.com>
2023-02-08 19:03:29 -0800

    Add name div

--- src/components/about/About.tsx
+++ src/components/about/About.tsx
@@ -6,6 +6,10 @@ import Intro from './Intro';
 function About() {
     return (
         <div className='about'>
+            <div className='name'>
+                <h1 className='first'>Cindy</h1>
+                <h1 className='last'>Phelps</h1>
+            </div>
             <div className='about-background'>
                 <Heading />
                 <Intro />
