01bb3ec16874ec43383d603e17f5ac705ada3c78
doozles411 <cindy.phelps01@gmail.com>
2023-02-08 16:00:27 -0800

    Add content

--- src/components/about/Heading.tsx
+++ src/components/about/Heading.tsx
@@ -6,7 +6,33 @@ import selfie from '../../assets/images/placeholder-selfie.jpeg';
 function Heading() {
     return (
         <div className='heading'>
-            placeholder
+            <div className='name'>
+                <h1 className='first'>Cindy</h1>
+                <h1 className='last'>Phelps</h1>
+            </div>
+            <div className='selfie'>
+                <div className='selfie-outline'></div>
+                <motion.img
+                    className='selfie-img'
+                    alt="Selfie"
+                    src={selfie}
+                    initial={{ y: 24, x: 24 }}
+                    animate={{ y: 0, x: 0 }}
+                    transition={{ duration: 1 }}
+                >
+                </motion.img>
+            </div>
+            <div className='job-title'>
+                <WindupChildren>
+                    <Pace getPace={(char) => (char === " " ? 100 : 40)}>
+                        <p>{"Hello! I am a"}</p>
+                    </Pace>
+                    <Pause ms={300} />
+                    <Pace getPace={(char) => (char === " " ? 200 : 70)}>
+                        <h2>{"Front-end Developer"}</h2>
+                    </Pace>
+                </WindupChildren>
+            </div>
         </div>
     )
 }
