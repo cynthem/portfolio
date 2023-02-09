1aa1df9e787d59345fc82a226fc4b6d23aa42aac
doozles411 <cindy.phelps01@gmail.com>
2023-02-08 16:18:07 -0800

    Add content

--- src/components/contact/Footer.tsx
+++ src/components/contact/Footer.tsx
@@ -5,6 +5,26 @@ import heartIcon from '../../assets/images/heart_icon.svg';
 function Footer() {
     return (
         <div className='footer'>
+            <p>
+                Made with 
+                <img 
+                    alt='love'
+                    src={heartIcon}
+                /> 
+                by 
+                <motion.a 
+                    href='https://github.com/doozles411' 
+                    target="_blank" 
+                    rel="noopener noreferrer"
+                    whileHover={{
+                        color: '#fefae9', 
+                        transition: { duration: 0.5 }
+                    }}
+                    whileTap={{ color: "red" }}
+                >
+                    CMP
+                </motion.a>
+            </p>
         </div>
     );
 }
