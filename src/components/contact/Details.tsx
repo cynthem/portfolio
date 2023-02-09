ae33fe67c6f82279ef32b1ab4370f29e74eef1aa
doozles411 <cindy.phelps01@gmail.com>
2023-02-08 17:37:21 -0800

    Add content

--- src/components/contact/Details.tsx
+++ src/components/contact/Details.tsx
@@ -7,6 +7,45 @@ import resumeIcon from '../../assets/images/contact-icons/resume_icon.svg';
 function Details() {
     return (
         <div className='details'>
+            <p>I am currently open to new opportunities. Please reach out if you're interested in working together, have any questions for me, or have something else to say!</p>
+            <div className='details-links'>
+                <motion.a 
+                    href="https://github.com/doozles411"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    whileHover={{
+                        scale: 1.2,
+                        transition: { duration: 0.5 }
+                    }}
+                    whileTap={{ scale: 0.9 }}
+                >
+                    <img alt="GitHub" src={gitHubIcon} />
+                </motion.a>
+                <motion.a 
+                    href="#"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    whileHover={{
+                        scale: 1.2,
+                        transition: { duration: 0.5 }
+                    }}
+                    whileTap={{ scale: 0.9 }}
+                >
+                    <img alt="LinkedIn" src={linkedInIcon} />
+                </motion.a>
+                <motion.a 
+                    href="#"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    whileHover={{
+                        scale: 1.2,
+                        transition: { duration: 0.5 }
+                    }}
+                    whileTap={{ scale: 0.9 }}
+                >
+                    <img alt="Resume" src={resumeIcon} />
+                </motion.a>    
+            </div>
         </div>
     );
 }
