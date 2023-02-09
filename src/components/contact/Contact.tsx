aa5e2d5a3744320cee9ae3ac730120e8d63d561f
doozles411 <cindy.phelps01@gmail.com>
2023-02-08 16:29:27 -0800

    Add content

--- src/components/contact/Contact.tsx
+++ src/components/contact/Contact.tsx
@@ -7,6 +7,14 @@ import EmailForm from './EmailForm';
 function Contact() {
     return (
         <div className='contact'>
+            <div className='contact-header' id='contact'>
+                <p className='number'>04.</p>
+                <h2>Contact</h2>
+            </div>
+            <div className='contact-details'>
+                <Details />
+                <EmailForm />
+            </div>
         </div>
     );
 }
