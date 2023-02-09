79a5718a621bd7605bb3944d497d1a93409c647c
doozles411 <cindy.phelps01@gmail.com>
2023-02-08 16:17:00 -0800

    Add content

--- src/components/projects/MoreProjects.tsx
+++ src/components/projects/MoreProjects.tsx
@@ -7,8 +7,18 @@ import arrowIcon from '../../assets/images/icon_arrow_orange.png';
 function MoreProjects() {
     return (
         <div className='more-projects'>
+            <div className='more-projects-background'>
+                <img 
+                    alt='More projects' 
+                    src={otherProjects}
+                />
+            </div>
+            <div className='more-projects-link'>
+                <h3>More projects</h3>
+                <img alt="Arrow icon" src={arrowIcon} />
+            </div>
         </div>
-    )
+    );
 }
 
 export default MoreProjects;
