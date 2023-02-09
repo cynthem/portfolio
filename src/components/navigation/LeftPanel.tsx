e596581af3e796ce494b2f553636414bc4745246
doozles411 <cindy.phelps01@gmail.com>
2023-02-08 16:39:24 -0800

    Add content

--- src/components/navigation/LeftPanel.tsx
+++ src/components/navigation/LeftPanel.tsx
@@ -2,8 +2,24 @@ import React, { useState, useEffect, Dispatch, SetStateAction } from 'react';
 import NavItem from './NavItem';
 
 function LeftPanel() {
+    const navItems: { [key: string]: number | null } = {
+        about: 4,
+        skills: null, 
+        projects: null, 
+        contact: null
+    }
+
+    const navList = Object.keys(navItems).map((e, i) => 
+        <NavItem 
+            key={`navitem_${i}`} 
+        />
+    );
+
     return (
         <div className='left-panel'>
+            <ul className='link-list'>
+                {navList}
+            </ul>
         </div>
     );
 }
