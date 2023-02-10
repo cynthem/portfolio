c22be7a6f19d5d60514cd20df910924691490e01
doozles411 <cindy.phelps01@gmail.com>
2023-02-10 14:34:32 -0800

    Move name div inside background div

--- src/components/about/About.tsx
+++ src/components/about/About.tsx
@@ -6,11 +6,11 @@ import Intro from './Intro';
 function About() {
     return (
         <div className='about'>
-            <div className='name'>
-                <h1 className='first'>Cindy</h1>
-                <h1 className='last'>Phelps</h1>
-            </div>
             <div className='about-background'>
+                <div className='name'>
+                    <h1 className='first'>Cindy</h1>
+                    <h1 className='last'>Phelps</h1>
+                </div>
                 <Heading />
                 <Intro />
             </div>
