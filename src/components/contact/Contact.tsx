56f2e711e0778e2331441f061e0710238f46dadb
doozles411 <cindy.phelps01@gmail.com>
2023-02-11 17:29:13 -0800

    Add animation to heading

--- src/components/contact/Contact.tsx
+++ src/components/contact/Contact.tsx
@@ -1,17 +1,47 @@
 import React, { useEffect } from 'react';
-import { useInView } from 'react-intersection-observer';
 import { motion, useAnimation } from 'framer-motion';
+import { useInView } from "react-intersection-observer";
 import Details from './Details';
 import EmailForm from './EmailForm';
 import Footer from './Footer';
 
 function Contact() {
+    const animation = useAnimation();    
+    const [ref, inView, entry] = useInView({ threshold: 0.1 });
+
+    useEffect(() => {
+      if (inView) {
+        animation.start("visible");
+      }
+    }, [animation, inView]);
+
+    const animateDetails = {
+        visible: {
+            y: 0,
+            opacity: 1,
+            staggerChildren: 1,
+            delayChildren: 1,
+            transition: { duration: 1 }
+        },
+        hidden: {
+            y: 100,
+            opacity: 0
+        }
+    }
+
     return (
         <div className='contact'>
-            <div className='contact-header' id='contact'>
+            <motion.div 
+                className='contact-header' 
+                id='contact'
+                ref={ref}
+                animate={animation}
+                initial={{opacity: 0}}
+                variants={animateDetails}
+            >
                 <p className='number'>04.</p>
                 <h2>Contact</h2>
-            </div>
+            </motion.div>
             <div className='contact-details'>
                 <Details />
                 <EmailForm />
