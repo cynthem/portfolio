1ccc14592ecee075a64fce5cd384136861f8d6e5
doozles411 <cindy.phelps01@gmail.com>
2023-02-11 17:09:45 -0800

    Add scroll animation

--- src/components/projects/Projects.tsx
+++ src/components/projects/Projects.tsx
@@ -1,14 +1,44 @@
 import React, { useState, useEffect } from 'react';
-import { motion } from 'framer-motion';
+import { motion, useAnimation } from 'framer-motion';
+import { useInView } from "react-intersection-observer";
 import uniqid from 'uniqid';
 import Project from './Project';
 import MoreProjects from './MoreProjects';
 import projectPreviews from '../../util/projectPreviews';
 
 function Projects() {
+    const animation = useAnimation();    
+    const [ref, inView, entry] = useInView({ threshold: 0 });
+
+    useEffect(() => {
+      if (inView) {
+        animation.start("visible");
+      }
+    }, [animation, inView]);
+
+    const animateDetails = {
+        visible: {
+            y: 0,
+            scale: 1,
+            staggerChildren: 0,
+            delayChildren: 0,
+            transition: { duration: 0.8 }
+        },
+        hidden: {
+            y: 100,
+            scale: 0.8
+        }
+    }
+
     return (
         <div className='projects' id='projects'>
-            <div className='projects-container'>
+            <motion.div 
+                className='projects-container'
+                ref={ref}
+                animate={animation}
+                initial={{scale: 0.8}}
+                variants={animateDetails}
+            >
                 <div className='projects-header'>
                     <p className='number'>03.</p>
                     <h2>Projects</h2>
@@ -24,7 +54,7 @@ function Projects() {
                     })}
                 </div>
                 <MoreProjects />
-            </div>
+            </motion.div>
         </div>
     );
 }
