dd7307affcc81ab6700966293c7665a4448facfb
doozles411 <cindy.phelps01@gmail.com>
2023-02-12 16:08:58 -0800

    Add button and image animations

--- src/components/contact/EmailForm.tsx
+++ src/components/contact/EmailForm.tsx
@@ -26,9 +26,40 @@ function EmailForm() {
                 }
             );
     }
-    
+
+    const animateBtn = {
+        rest: {
+            scale: 1,
+            color: 'black',
+            transition: { duration: 0.5 }
+        },
+        hover: {
+            scale: 1.2,
+            color: '#ffa600', 
+            transition: { duration: 0.5 }
+        },
+        tap: {
+            scale: 0.9
+        }
+    }
+
+    const animateIcon = {
+        rest: {
+            rotate: 0,
+            transition: { duration: 0.5 }
+        },
+        hover: {
+            rotate: 45,
+            transition: { duration: 0.5 }
+        }
+    }
+
     return (
-        <form className='email-form'>
+        <form 
+            className='email-form'
+            ref={form as React.RefObject<HTMLFormElement>}
+            onSubmit={sendEmail}
+        >
             <div className='form-inputs'>
                 <input 
                     className='name-input' 
@@ -54,18 +85,24 @@ function EmailForm() {
                     required
                 />
             </div>
-            <button 
+            <motion.button 
                 className='send-input'
                 type='submit'
                 value='Send'
                 aria-label='Send email'
+                variants={animateBtn}
+                initial="rest"
+                whileHover="hover"
+                whileTap="tap"
+                animate="rest"
             >
                 Send
-                <img 
+                <motion.img 
                     alt='Send' 
                     src={arrowIcon} 
+                    variants={animateIcon}
                 />
-            </button>
+            </motion.button>
         </form>
     );
 }
