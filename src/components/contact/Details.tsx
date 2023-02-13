30d87a3fb8207fc40117fcc1e969b5e6c574f040
doozles411 <cindy.phelps01@gmail.com>
2023-02-12 16:03:03 -0800

    Remove opacity animation

--- src/components/contact/Details.tsx
+++ src/components/contact/Details.tsx
@@ -1,42 +1,12 @@
-import React, { useEffect } from 'react';
-import { motion, useAnimation } from 'framer-motion';
-import { useInView } from "react-intersection-observer";
+import React from 'react';
+import { motion } from 'framer-motion';
 import gitHubIcon from '../../assets/images/contact-icons/github_icon.svg';
 import linkedInIcon from '../../assets/images/contact-icons/linkedin_icon.svg';
 import resumeIcon from '../../assets/images/contact-icons/resume_icon.svg';
 
 function Details() {
-    const animation = useAnimation();    
-    const [ref, inView, entry] = useInView({ threshold: 0.1 });
-
-    useEffect(() => {
-      if (inView) {
-        animation.start("visible");
-      }
-    }, [animation, inView]);
-
-    const animateDetails = {
-        visible: {
-            y: 0,
-            opacity: 1,
-            staggerChildren: 1,
-            delayChildren: 1,
-            transition: { duration: 1 }
-        },
-        hidden: {
-            y: 100,
-            opacity: 0
-        }
-    }
-
     return (
-        <motion.div 
-            className='details'
-            //ref={ref}
-            //animate={animation}
-            //initial={{opacity: 0}}
-            //variants={animateDetails}
-        >
+        <div className='details'>
             <p>I am currently open to new opportunities. Don't hesitate to reach out if you're interested in working together or have any questions for me.</p>
             <div className='details-links'>
                 <motion.a 
@@ -76,7 +46,7 @@ function Details() {
                     <img alt="Resume" src={resumeIcon} />
                 </motion.a>    
             </div>
-        </motion.div>
+        </div>
     );
 }
 
