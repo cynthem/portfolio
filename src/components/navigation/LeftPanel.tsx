bb61230e63dd5f441d9a6e38edcd56e16908a41e
doozles411 <cindy.phelps01@gmail.com>
2023-02-27 14:06:15 -0800

    Remove unused imports

--- src/components/navigation/LeftPanel.tsx
+++ src/components/navigation/LeftPanel.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, Dispatch, SetStateAction } from 'react';
+import React, { useState, useEffect } from 'react';
 import NavItem from './NavItem';
 
 function LeftPanel() {
