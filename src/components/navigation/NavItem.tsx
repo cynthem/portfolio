e38ae239fc3271c9597eb43dc27ad92a8ac8efb4
doozles411 <cindy.phelps01@gmail.com>
2023-02-27 13:50:42 -0800

    Pass currentPage prop

--- src/components/navigation/NavItem.tsx
+++ src/components/navigation/NavItem.tsx
@@ -3,10 +3,10 @@ import React, { useState, useEffect } from 'react';
 interface Props {
     navName: string;
     active: boolean;
-    //currentPage: string;
+    currentPage: string;
 }
 
-function NavItem({ navName, active }: Props) {
+function NavItem({ navName, active, currentPage }: Props) {
     const [anchorTarget, setAnchorTarget] = useState<HTMLElement | null>(null);
 
     useEffect(() => {
