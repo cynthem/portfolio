bd80b4f662e0d84ce36f065a09cef81520f6836f
doozles411 <cindy.phelps01@gmail.com>
2023-02-27 15:04:34 -0800

    Move scroll tracking logic from LeftPanel

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -6,16 +6,61 @@ import Projects from './projects/Projects';
 import Contact from './contact/Contact';
 import '../sass/main.scss';
 
+const navItems: { [key: string]: number | null } = {
+  about: null,
+  skills: null, 
+  projects: null, 
+  contact: null
+}
+
 export function App() {
-  return (
-    <div className='app'>
-        <LeftPanel />
-        <div className='pages'>
-            <About />
-            <Skills />
-            <Projects />
-            <Contact />
+    const [activeNav, setActiveNav] = useState('Top');
+
+    useEffect(() => {
+        const observer = new MutationObserver(getAnchorPoints);
+        observer.observe(document.getElementById('app')!, {
+            childList: true,
+            subtree: true
+        });
+        window.addEventListener('scroll', handleScroll);
+    }, []);
+
+    const getAnchorPoints = () => {
+        const currentScroll = window.scrollY - 100;
+        /*const viewportHeight = Math.max(
+            document.documentElement.clientHeight,
+            window.innerHeight || 0
+        );*/
+        for (const key in navItems) {
+            navItems[key] = document.getElementById(key)?.getBoundingClientRect().top! + currentScroll;
+        }
+        //const bottom = document.body.offsetHeight;
+        handleScroll();
+    }
+
+    const handleScroll = () => {
+        const currentPosition = window.scrollY;
+        let currentSection = null;
+        for (const section in navItems) {
+            currentSection = navItems[section]! <= currentPosition ? section : currentSection;
+            if (currentSection !== section) {
+                break;
+            }
+        }
+        if (currentSection !== activeNav) {
+            setActiveNav(currentSection!);
+        }
+    }
+
+    return (
+        <div className="app">
+            <LeftPanel />
+            <div className="pages">
+                <About />
+                <Skills />
+                <Projects />
+                <Contact />
+            </div>
         </div>
-    </div>
-  );
+    );
 }
--- src/components/navigation/LeftPanel.tsx
+++ src/components/navigation/LeftPanel.tsx
@@ -1,51 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import NavItem from './NavItem';
 
-const navItems: { [key: string]: number | null } = {
-    about: 4,
-    skills: null, 
-    projects: null, 
-    contact: null
-}
-
 function LeftPanel() {
-    const [activeNav, setActiveNav] = useState('Top');
-
-    useEffect(() => {
-        const observer = new MutationObserver(getAnchorPoints);
-        observer.observe(document.getElementById('app')!, {
-            childList: true,
-            subtree: true
-        });
-        window.addEventListener('scroll', handleScroll);
-    }, []);
-
-    const getAnchorPoints = () => {
-        const currentScroll = window.scrollY - 100;
-        const viewportHeight = Math.max(
-            document.documentElement.clientHeight,
-            window.innerHeight || 0
-        );
-        for (const key in navItems) {
-            navItems[key] = document.getElementById(key)?.getBoundingClientRect().top! + currentScroll;
-        }
-        const bottom = document.body.offsetHeight;
-        handleScroll();
-    }
-
-    const handleScroll = () => {
-        const currentPosition = window.scrollY;
-        let currentSection = null;
-        for (const section in navItems) {
-            currentSection = navItems[section]! <= currentPosition ? section : currentSection;
-            if (currentSection !== section) {
-                break;
-            }
-        }
-        if (currentSection !== activeNav) {
-            setActiveNav(currentSection!);
-        }
-    }
 
     const navList = Object.keys(navItems).map((e, i) => 
         <NavItem 
