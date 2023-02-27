7ad4a6691e9746a6c6a0870d63a4e530313eb9f5
doozles411 <cindy.phelps01@gmail.com>
2023-02-27 13:39:07 -0800

    Add scroll measuring logic

--- src/components/navigation/LeftPanel.tsx
+++ src/components/navigation/LeftPanel.tsx
@@ -2,6 +2,44 @@ import React, { useState, useEffect, Dispatch, SetStateAction } from 'react';
 import NavItem from './NavItem';
 
 function LeftPanel() {
+    const [activeNav, setActiveNav] = useState('Top');
+
+    useEffect(() => {
+        const observer = new MutationObserver(getAnchorPoints);
+        observer.observe(document.getElementById('app')!, {
+            childList: true,
+            subtree: true
+        });
+        window.addEventListener('scroll', handleScroll);
+    }, []);
+
+    const getAnchorPoints = () => {
+        const currentScroll = window.scrollY - 100;
+        const viewportHeight = Math.max(
+            document.documentElement.clientHeight,
+            window.innerHeight || 0
+        );
+        for (const key in navItems) {
+            navItems[key] = document.getElementById(key)?.getBoundingClientRect().top! + currentScroll;
+        }
+        const bottom = document.body.offsetHeight;
+        handleScroll();
+    }
+
+    const handleScroll = () => {
+        const currentPosition = window.scrollY;
+        let currentSection = null;
+        for (const section in navItems) {
+            currentSection = navItems[section]! <= currentPosition ? section : currentSection;
+            if (currentSection !== section) {
+                break;
+            }
+        }
+        if (currentSection !== activeNav) {
+            setActiveNav(currentSection!);
+        }
+    }
+
     const navItems: { [key: string]: number | null } = {
         about: 4,
         skills: null, 
@@ -11,8 +49,10 @@ function LeftPanel() {
 
     const navList = Object.keys(navItems).map((e, i) => 
         <NavItem 
-            navName={e}
-            key={`navitem_${i}`}
+            navName={e} 
+            key={`navitem_${i}`} 
+            active={e === activeNav ? true : false} 
+            currentPage={currentPage} 
         />
     );
 
