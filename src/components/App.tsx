fbf0653dc998a066ecf954a14ca55444b8d38095
doozles411 <cindy.phelps01@gmail.com>
2023-02-27 18:55:42 -0800

    Adjust scroll measuring logic

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -6,29 +6,28 @@ import Projects from './projects/Projects';
 import Contact from './contact/Contact';
 import '../sass/main.scss';
 
+const navItems = {
+    about: 0,
+    skills: 0,
+    projects: 0,
+    contact: 0
+}
+
 export function App() {
     const [currentPage, setCurrentPage] = useState("home");
     const [scrollView, setScrollView] = useState(0);
     const inView = useRef<HTMLDivElement>(null);
-    
-    const navItems = {
-        about: 0,
-        skills: 0,
-        projects: 0,
-        contact: 0
-    }
 
     const scrollWatcher = () => {
         if (!inView.current) return;
         const scrollLocation = (inView.current as HTMLElement).scrollTop;
-        //const totalHeight = (inView.current as HTMLElement).scrollHeight - (inView.current as HTMLElement).clientHeight;
+        const totalHeight = (inView.current as HTMLElement).scrollHeight - (inView.current as HTMLElement).clientHeight;
         if (scrollLocation === 0 ) return setScrollView(0);
-        //if (scrollLocation > totalHeight) return setScrollView(100);
-        setScrollView(scrollLocation);
-        console.log(scrollView)
-      }
+        if (scrollLocation > totalHeight) return setScrollView(100);
+        setScrollView((scrollLocation / totalHeight) * 100);
+    }
     
-      useEffect(() => {
+    useEffect(() => {
         inView.current?.addEventListener('scroll', scrollWatcher);
         return () => {
             inView.current &&
@@ -37,20 +36,12 @@ export function App() {
     });
 
     useEffect(() => {
-        const observer = new MutationObserver(getAnchorPoints);
-        observer.observe(document.getElementById('app')!, {
-            childList: true,
-            subtree: true
-        });
-    }, []);
-
-    const getAnchorPoints = () => {
-        const currentScroll = window.scrollY;
         for (const key in navItems) {
-            navItems[key as keyof typeof navItems] = document.getElementById(key)?.getBoundingClientRect().top! + currentScroll;
+            const componentTop = document.getElementById(key)?.getBoundingClientRect().top!;
+            const appHeight = (inView.current as HTMLElement).scrollHeight;
+            navItems[key as keyof typeof navItems] = (componentTop / appHeight) * 100;
         }
-        //console.log(navItems)
-    }
+    }, []);
 
     return (
         <div className="app" ref={inView as React.RefObject<HTMLDivElement>}>
