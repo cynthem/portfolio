d5668bcdf82116c3abe0c218b48f58951e8ab933
doozles411 <cindy.phelps01@gmail.com>
2023-02-27 18:56:26 -0800

    Remove unused import

--- src/components/navigation/NavItem.tsx
+++ src/components/navigation/NavItem.tsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { motion, useAnimation } from 'framer-motion';
+import { motion } from 'framer-motion';
 
 interface Props {
     navName: string;
@@ -24,9 +24,7 @@ function NavItem({ navName, active, currentPage }: Props) {
     }
 
     return (
-        <motion.li
-            whileTap={{ scale: 0.9 }}
-        >
+        <motion.li whileTap={{ scale: 0.9 }}>
             <motion.a 
                 href={`#${navName}`}
                 className={`underline ${active} ${currentPage === `${navName}` && `${navName}-nav`}`}
