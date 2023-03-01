b0306107983bb7cd1a1315d4c395fbe958350cc3
doozles411 <cindy.phelps01@gmail.com>
2023-02-28 17:51:20 -0800

    Remove unused imports

--- src/components/projects/MoreProjects.tsx
+++ src/components/projects/MoreProjects.tsx
@@ -1,6 +1,6 @@
-import React, { useRef } from 'react';
+import React from 'react';
 import { Parallax, ParallaxProvider } from 'react-scroll-parallax';
-import { motion, useScroll, useTransform } from 'framer-motion';
+import { motion } from 'framer-motion';
 import otherProjects from '../../assets/images/project-imgs/otherProj.jpg';
 import arrowIcon from '../../assets/images/icon_arrow_orange.png';
 
