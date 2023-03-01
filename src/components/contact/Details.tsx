39e66396b65f4f5951cab91a658e9aa2a08e893b
doozles411 <cindy.phelps01@gmail.com>
2023-03-01 15:18:13 -0800

    Install React Router

--- src/components/contact/Details.tsx
+++ src/components/contact/Details.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { motion } from 'framer-motion';
+import Resume from './Resume';
 import gitHubIcon from '../../assets/images/contact-icons/github_icon.svg';
 import linkedInIcon from '../../assets/images/contact-icons/linkedin_icon.svg';
 import resumeIcon from '../../assets/images/contact-icons/resume_icon.svg';
