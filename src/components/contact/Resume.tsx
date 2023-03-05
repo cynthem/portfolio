8d89eeed86b80000bff34e7065d91f3c214628db
doozles411 <cindy.phelps01@gmail.com>
2023-03-05 15:36:15 -0800

    Return main div with placeholder content

--- src/components/contact/Resume.tsx
+++ src/components/contact/Resume.tsx
@@ -1,7 +1,11 @@
 import React from 'react';
 
 function Resume() {
-
+    return (
+        <div className='resume'>
+            resume content placeholder
+        </div>
+    );
 }
 
 export default Resume;
