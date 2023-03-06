19b9f684180d95227b94ace447c5d96f6d94a098
doozles411 <cindy.phelps01@gmail.com>
2023-03-05 18:16:22 -0800

    Add handleClick prop

--- src/components/contact/Resume.tsx
+++ src/components/contact/Resume.tsx
@@ -1,15 +1,20 @@
 import React from 'react';
 
 interface Props {
+    handleClick: () => void;
     resumeShown: boolean;
 }
 
-function Resume({ resumeShown }: Props) {
+function Resume({ handleClick, resumeShown }: Props) {
     return (
         <div className={`resume ${resumeShown ? 'shown' : 'not-shown'}`}>
             <div className='resume-container'>
                 here is some content
-                <button>Exit</button>
+                <button
+                    onClick={handleClick}
+                >
+                    Exit
+                </button>
             </div>
         </div>
     );
