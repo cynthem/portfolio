745cefe0fc074fb32a396c16200cf82f8cd25c54
doozles411 <cindy.phelps01@gmail.com>
2023-04-25 12:03:06 -0700

    Edit text

--- src/components/about/Intro.tsx
+++ src/components/about/Intro.tsx
@@ -8,7 +8,7 @@ function Intro() {
                 <h2>About</h2>
             </div>
             <div className='intro-details'>
-                <p className='top-details'>Welcome! I am a self-taught front-end developer who spent 2022 learning to code and build web pages - and I'm so glad I did as web development has provided the professional spark that had been missing from my past experiences. I've never been as absorbed in anything as I have in solving a programming problem or building a web page from scratch.</p>
+                <p className='top-details'>Hi! I am a self-taught front-end developer who spent 2022 learning to code and build web pages - and I'm so glad I did as web development has provided the professional spark that had been missing from my past experiences. I've never been as absorbed in anything as I have in solving a programming problem or building a web page from scratch.</p>
                 <p>Having built dozens of websites and learned several languages and frameworks, I now look forward to bringing my passion for front-end development to a team doing interesting work. In the meantime, I'll be tackling projects from my wish list and tinkering with new libraries.</p>
             </div>
         </div>
