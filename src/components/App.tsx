794b2e5be21ab67015bb1f8aca62e98e7509d7f1
doozles411 <cindy.phelps01@gmail.com>
2023-04-25 14:22:21 -0700

    Import scss

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -4,7 +4,7 @@ import About from './about/About';
 import Skills from './skills/Skills';
 import Projects from './projects/Projects';
 import Contact from './contact/Contact';
-import '../sass/main';
+//import '../sass/main';
 
 const navItems = {
     about: 0,
