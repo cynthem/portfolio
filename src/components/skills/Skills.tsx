d31ef5068d1d498351ee27b6e460622ef3fcc352
doozles411 <cindy.phelps01@gmail.com>
2024-01-18 15:59:09 -0800

    Rearrange skills icons

--- src/components/skills/Skills.tsx
+++ src/components/skills/Skills.tsx
@@ -59,8 +59,8 @@ function Skills() {
                             <p>TypeScript</p>
                         </div>
                         <div className='skill'>
-                            <img alt="jQuery" src={skillIcons[9]} />
-                            <p>jQuery</p>
+                            <img alt="Next" src={skillIcons[18]} />
+                            <p>Next JS</p>
                         </div>
                         <div className='skill'>
                             <img alt="React" src={skillIcons[12]} />
@@ -73,10 +73,28 @@ function Skills() {
                             <p>Redux</p>
                         </div>
                         <div className='skill'>
+                            <img alt="jQuery" src={skillIcons[9]} />
+                            <p>jQuery</p>
+                        </div>
+                        <div className='skill'>
                             <img alt="Python" src={skillIcons[11]} />
                             <p>Python</p>
                         </div>
                         <div className='skill'>
+                            <img alt="MongoDB" src={skillIcons[19]} />
+                            <p>MongoDB</p>
+                        </div>
+                        <div className='skill'>
+                            <img alt="Node.js" src={skillIcons[10]} />
+                            <p>Node.js</p>
+                        </div>
+                        <div className='skill'>
+                            <img alt="Jest" src={skillIcons[8]} />
+                            <p>Jest</p>
+                        </div>
+                    </div>
+                    <div className='skills-row-3'>
+                    <div className='skill'>
                             <img alt="Sass" src={skillIcons[14]} />
                             <p>Sass</p>
                         </div>
@@ -86,33 +104,16 @@ function Skills() {
                         </div>
                         <div className='skill'>
                             <img alt="Styled Components" src={skillIcons[15]} />
-                            <p>Styled Components</p>
-                        </div>
-                        <div className='skill'>
-                            <img alt="Jest" src={skillIcons[8]} />
-                            <p>Jest</p>
-                        </div>
-                    </div>
-                    <div className='skills-row-3'>
-                        <div className='skill'>
-                            <img alt="Framer Motion" src={skillIcons[3]} />
-                            <p>Framer Motion</p>
+                            <p>Styled</p>
+                            <p>Components</p>
                         </div>
                         <div className='skill'>
                             <img alt="Git" src={skillIcons[4]} />
                             <p>Git</p>
                         </div>
                         <div className='skill'>
-                            <img alt="GitHub" src={skillIcons[5]} />
-                            <p>GitHub</p>
-                        </div>
-                        <div className='skill'>
                             <img alt="Webpack" src={skillIcons[17]} />
                             <p>Webpack</p>
-                        </div>
-                        <div className='skill'>
-                            <img alt="Node.js" src={skillIcons[10]} />
-                            <p>Node.js</p>
                         </div>
                         <div className='skill'>
                             <img alt="Firebase" src={skillIcons[2]} />
